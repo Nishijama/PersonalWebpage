27e4d3b5f3219102d7e83834218ba859296ba551
Nishijama <nathanplociennik@gmail.com>
2022-05-13 23:18:48 +0200

    Added firsts draft for blog

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("loaded");
-   let AboutIcon = document.getElementById('AboutIcon');
-   let AboutTooltip = document.getElementById('AboutTooltip');
-   AboutIcon.addEventListener('mouseover', () => {
-        AboutTooltip.style.display = "inline-block";
-    });
-    AboutIcon.addEventListener('mouseleave', () => {
-        AboutTooltip.style.display = "none";
-    });
-    GitLogo.addEventListener('mouseover', () => {
-        GitTooltip.style.display = "inline-block";
-    });
-    GitLogo.addEventListener('mouseleave', () => {
-        GitTooltip.style.display = "none";
-    });
+    let AboutIcon = document.getElementById('AboutIcon');
+    let AboutTooltip = document.getElementById('AboutTooltip');
+    let x = window.matchMedia("(min-width: 900px)")
+    addToolTips(x)
+    x.addListener(addToolTips)
 });
+
+
+function addToolTips(x){
+    if (x.matches) { // If media query matches
+        AboutIcon.addEventListener('mouseenter', () => {
+            AboutTooltip.style.display = "inline-block";
+        });
+        AboutIcon.addEventListener('mouseout', () => {
+            AboutTooltip.style.display = "none";
+        });
+        GitLogo.addEventListener('mouseenter', () => {
+            GitTooltip.style.display = "inline-block";
+        });
+        GitLogo.addEventListener('mouseout', () => {
+            GitTooltip.style.display = "none";
+        });
+        EmailIcon.addEventListener('mouseenter', () => {
+            EmailTooltip.style.display = "inline-block";
+        });
+        EmailIcon.addEventListener('mouseout', () => {
+            EmailTooltip.style.display = "none";
+        });
+        BlogIcon.addEventListener('mouseenter', () => {
+            BlogTooltip.style.display = "inline-block";
+        });
+        BlogIcon.addEventListener('mouseout', () => {
+            BlogTooltip.style.display = "none";
+        });
+    }
+}
